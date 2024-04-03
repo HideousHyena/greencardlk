379b57858df0c093f4d5a0718180d7326b769a4a
SinhalaKello <160373045+HideousHyena@users.noreply.github.com>
2024-04-03 07:42:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const images = document.querySelectorAll('#gallery img');
+    const gallery = document.getElementById('gallery');
+    const leftArrow = document.getElementById('left-arrow');
+    const rightArrow = document.getElementById('right-arrow');
 
+    leftArrow.addEventListener('click', function() {
+        gallery.scrollLeft -= 200; // Adjust the scroll value as needed
+    });
+
+    rightArrow.addEventListener('click', function() {
+        gallery.scrollLeft += 200; // Adjust the scroll value as needed
+    });
+
+    const images = gallery.querySelectorAll('img');
     images.forEach(image => {
         image.addEventListener('click', function () {
             const url = this.getAttribute('data-url');
